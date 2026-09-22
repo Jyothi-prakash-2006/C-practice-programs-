56bd4ae7a0f5b39f818185e5f44a663a44dcda43
Jyothi prakash <jyothiprakashprakash673@gmail.com>
2026-09-22 17:33:04 +0530

    Add program to find second largest number

---
+++ Finding the second largest number.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+
+    cout << "Enter 5 numbers: ";
+
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+    int secondLargest = arr[0];
+
+    for (int i = 1; i < 5; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    cout << "\nLargest number = " << largest << endl;
+    cout << "Second largest number = " << secondLargest << endl;
+
+    return 0;
+}
