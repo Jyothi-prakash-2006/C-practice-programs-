c1e7062c25eda537d8f77664e724052f6ed7b8be
Jyothi prakash <jyothiprakashprakash673@gmail.com>
2026-09-25 20:01:00 +0530

    Create Fibonacci series .cpp

---
+++ Fibonacci series .cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    int first = 0;
+    int second = 1;
+
+    cout << "\nFibonacci Series:\n";
+
+    for (int i = 1; i <= n; i++) {
+        cout << first << " ";
+
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+
+    cout << endl;
+
+    return 0;
+}
